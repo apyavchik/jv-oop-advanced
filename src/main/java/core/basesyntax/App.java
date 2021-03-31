1bf646aa3cf8b794f91b3da767e2191ef711756d
apyavchik <a.pyavchik@gmail.com>
2021-03-31 23:42:00 +0300

    add home work jv-oop-advanced

---
+++ src/main/java/core/basesyntax/App.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+//import core.basesyntax.utils.fabrica.FigureSupplier;
+//import core.basesyntax.utils.Old_FigureSupplier;
+
+import core.basesyntax.utils.factory.FigureSupplier;
+
+public class App {
+    public static void main(String[] args) {
+        Figure[] figures = new FigureSupplier().generateRandomFigures();
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
+}
